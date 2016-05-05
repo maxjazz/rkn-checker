0f0887bf4a1f943aa66476dca7db054e5a9e8b77
Maksim Prokopev <maksim.prokopev@jambook.ru>
2016-05-05 12:32:14 +0300

    Updated structures

---
+++ settings/rkn.py
@@ -0,0 +1,9 @@
+OPERATOR_NAME = "OPERATOR"
+OPERATOR_INN = "12345678910"
+OPERATOR_OGRN = "12345678910"
+OPERATOR_EMAIL = "mail@operator.ru"
+
+WORK_DIR = "/home/cmd4jazz/roskomnadzor/"
+
+RKN_PID="/var/log/rkn.pid"
+RKN_LOG="/var/log/rkn.message"
