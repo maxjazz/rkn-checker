91ec489ec54d16f615021e3d46bc51500a710dc2
Maksim Prokopev <maksim.prokopev@jambook.ru>
2016-11-22 13:27:17 +0300

    Update openssl-sign.sh

--- scripts/openssl-sign.sh
+++ scripts/openssl-sign.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
+DIRECTORY="/openssl-gost/"
+KEY="keys/letus.pem"
+
+
+function backup
+{
+    echo "backup";
+}
+
+function sign
+{
+    $DIRECTORY/bin/openssl smime -sign -in request.xml -out request.xml.sign -signer $KEY -outform DER
+}
+
+function info
+{
+    echo "info";
+}
+
+function test
+{
+    echo "test";
+}
+
+function help
+{
+    echo "Usage: $0 [backup] [sign] [info] [test]";
+}
+
+
 case $1 in 
 backup) backup;;
 sign) sign;;
 info) info;;
 test) test;;
+*) help;;
 esac
--- sign.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/openssl-gost/bin/openssl smime -sign -in ./request.xml -out ./request.xml.sign -signer keys/letus.pem -outform DER
