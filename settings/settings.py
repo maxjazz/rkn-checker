857026f63ef35dbc04ba73572e967addf510cc56
Maksim Prokopev <maxim@jambook.ru>
2016-11-23 13:20:08 +0300

    Update Settings

--- settings/settings.py
+++ settings/settings.py
@@ -1,10 +1,17 @@
+
+# Operator Settings 
 OPERATOR_NAME = "OPERATOR"
 OPERATOR_INN = "12345678910"
 OPERATOR_OGRN = "12345678910"
 OPERATOR_EMAIL = "mail@operator.ru"
 OPERATOR_TIMEZONE = "Europe/Moscow"
 
-WORK_DIR = "."
 
+# Log file and rkn.pid directory 
+WORK_DIR = "."
 RKN_PID="/rkn.pid"
 RKN_LOG="rkn.message"
+
+
+# Settings for RKN services
+URL_API="http://vigruzki.rkn.gov.ru/services/OperatorRequest/?wsdl"
