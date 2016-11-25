76b32ed84121cd6f5a1ce8acf197821357f386ab
Maksim Prokopev <maksim.prokopev@jambook.ru>
2016-11-25 15:16:22 +0300

    Add commande line parse

--- rkn.py
+++ rkn.py
@@ -3,6 +3,7 @@ import sys
 import logging
 import os
 import re
+import argparse
 
 #import psutil
 
@@ -22,10 +23,29 @@ logging.basicConfig(level=logging.DEBUG,
                     filename=settings.RKN_LOG,
                     filemode='a')
 
-if __name__ == "__main__":
+def parser():
+    rknParser = argparse.ArgumentParser()
+    rknSubparser = rknParser.add_subparsers()
 
-    # 1. Проверка наличия всех зависимостей
-    # 2. Проверка запущенного rknDaemon()
+    # Start RKN
+    start_parser = rknSubparser.add_parser('start', help='Starting daemon')
+    start_parser.set_defaults(start = True, func = start)
+
+    # Stop RKN
+    stop_parser = rknSubparser.add_parser('stop', help='Stop daemon')
+    stop_parser.set_defaults(func = stop)
+
+    # Reload RKN
+    reload_parser = rknSubparser.add_parser('reload', help='reload daemon')
+    reload_parser.set_defaults(func = reload)
+
+    # Show status of RKN
+    status_parser = rknSubparser.add_parser('status', help='Show status')
+    status_parser.set_defaults(func = status)
+
+    return rknParser
+
+def start():
     f = open(settings.RKN_PID, 'r')
     pid = f.read();
     print ('/proc/'+pid);
@@ -36,3 +56,22 @@ if __name__ == "__main__":
         logging.debug ("Process with pid = %s does not exist", pid);
         rkn = rknDaemon(settings.RKN_PID)
         rkn.start()
+
+def stop():
+    print ("Stop")
+
+def reload():
+    print ("Reload")
+
+def status():
+    print ("Status")
+
+if __name__ == "__main__":
+
+    rknArgs = parser().parse_args()
+
+    if len(sys.argv) == 1:
+        start()
+    else:
+        rknArgs.func()
+    
